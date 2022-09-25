4da57a24300659204d5618f2be551429053b8190
apromac <abraham.tiene@apromac.ci>
2022-09-25 09:56:12 +0000

    Mise a jour de l implementation du microservice saigneur-candidat

--- src/main/java/com/apromac/saigneur/serviceimpl/LocaliteCandidatServiceImpl.java
+++ src/main/java/com/apromac/saigneur/serviceimpl/LocaliteCandidatServiceImpl.java
@@ -15,11 +15,12 @@ public class LocaliteCandidatServiceImpl implements LocaliteCandidatService {
      * @return
      */
     public LocaliteCandidatDTO findByPosteTDH(OccuperBean occuperBean) {
+
         LocaliteCandidatDTO localiteCandidatDTO = new LocaliteCandidatDTO();
 
-        localiteCandidatDTO.setPosteTDH(occuperBean.getPosteBean().getLibellePoste());
-        localiteCandidatDTO.setNomTDH(occuperBean.getUtilisateurBean().getNomUtilisateur());
-        localiteCandidatDTO.setPrenomsTDH(occuperBean.getUtilisateurBean().getPrenomsUtilisateur());
+//        localiteCandidatDTO.setPosteTDH(occuperBean.getPosteBean().getLibellePoste());
+//        localiteCandidatDTO.setNomTDH(occuperBean.getUtilisateurBean().getNomUtilisateur());
+//        localiteCandidatDTO.setPrenomsTDH(occuperBean.getUtilisateurBean().getPrenomsUtilisateur());
         localiteCandidatDTO.setDistrictTDH(occuperBean.getDistrictOccuper());
         localiteCandidatDTO.setZoneTDH(occuperBean.getZoneOccuper());
 
