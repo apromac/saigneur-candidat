46b53e227ad15db2090b3ef3321a44eae0c24d0a
apromac <abraham.tiene@apromac.ci>
2022-10-04 17:56:34 +0000

    update files

--- src/main/java/com/apromac/saigneur/SaigneurCandidatApplication.java
+++ src/main/java/com/apromac/saigneur/SaigneurCandidatApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @EnableEurekaClient
 @EnableFeignClients("com.apromac.saigneur")
 public class SaigneurCandidatApplication {
