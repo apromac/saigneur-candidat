3ac6738f1aa4eccfd815985bcbf23544108b695a
apromac <abraham.tiene@apromac.ci>
2022-10-14 18:23:39 +0000

    Update candidat microservice implementation

--- src/main/java/com/apromac/saigneur/bean/UtilisateurBean.java
+++ src/main/java/com/apromac/saigneur/bean/UtilisateurBean.java
@@ -3,10 +3,21 @@ package com.apromac.saigneur.bean;
 import lombok.Getter;
 import lombok.Setter;
 
+
 @Getter
 @Setter
 public class UtilisateurBean {
+//    private Long utilisateurID;
+//    private String nomUtilisateur;
+//    private String prenomsUtilisateur;
+
+
+
     private Long utilisateurID;
     private String nomUtilisateur;
     private String prenomsUtilisateur;
+    private String username;
+    private String password;
+    private String telephoneUtilisateur;
+    private String photoUtilisateur;
 }
