8f0442987ac6315d695915e2ad113d861ac92d03
apromac <abraham.tiene@apromac.ci>
2022-10-16 10:10:02 +0000

    Update candidat microservice implementation

--- src/main/java/com/apromac/saigneur/SaigneurCandidatApplication.java
+++ src/main/java/com/apromac/saigneur/SaigneurCandidatApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 @SpringBootApplication
-@EnableWebMvc
 //@EnableDiscoveryClient
 @EnableEurekaClient
 @EnableFeignClients("com.apromac.saigneur")
