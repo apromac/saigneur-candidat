1e810a69a13e0c54f8fc7a3ec1e22c7da9962e1e
apromac <abraham.tiene@apromac.ci>
2022-10-16 09:36:20 +0000

    Update candidat microservice implementation

--- src/main/java/com/apromac/saigneur/configuration/SwaggerConfig.java
+++ src/main/java/com/apromac/saigneur/configuration/SwaggerConfig.java
@@ -13,7 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import java.util.Collections;
 
 @Configuration
-@EnableSwagger2
+//@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
