ca7dce743993ea2150a0ee87d37cde0b07487cb8
apromac <abraham.tiene@apromac.ci>
2022-11-18 18:44:36 +0000

    Implementation saigneur-candidat

--- src/main/java/com/apromac/saigneur/configuration/SwaggerConfig.java
+++ src/main/java/com/apromac/saigneur/configuration/SwaggerConfig.java
@@ -9,6 +9,8 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 import java.util.Collections;
 
